2e4ccc42ee5008595e9380d9c078130967a69923
Ajeesh B Nair <ajeeshklr@gmail.com>
2020-04-16 22:33:22 +0530

    Saving samples

---
+++ Kafka_Samples/KafkaConsumer/KafkaConsumer/Program.cs
@@ -0,0 +1,91 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                StatisticsIntervalMs = 1200000,
+              //  QueuedMinMessages = 1000,
+                FetchWaitMaxMs = 5,
+                FetchErrorBackoffMs = 10,
+                EnableAutoCommit = false,
+                SocketReceiveBufferBytes = 1048576,
+               // Debug="consumer,topic",
+               // AutoCommitIntervalMs = 2,
+                // Note: The AutoOffsetReset property determines the start offset in the event
+                // there are not yet any committed offsets for the consumer group for the
+                // topic/partitions of interest. By default, offsets are committed
+                // automatically, so in this example, consumption will only start from the
+                // earliest message in the topic 'my-topic' the first time you run the program.
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            var commitPeriod = 5; // in ms ( for manual commit operation )
+
+            using (var c = new ConsumerBuilder<Ignore, string>(conf)
+                .SetStatisticsHandler((_, json) => Console.WriteLine($"Statistics: {json}"))
+                .Build())
+            {
+                c.Subscribe("my-topic");
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true; // prevent the process from terminating.
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+                            if (cr.IsPartitionEOF)
+                            {
+                                Console.WriteLine(
+                                    $"Reached end of topic {cr.Topic}, partition {cr.Partition}, offset {cr.Offset}.");
+
+                                continue;
+                            }
+
+                            Console.WriteLine($"Consumed message at: '{cr.TopicPartitionOffset}'.");
+
+                            if (cr.Offset % commitPeriod == 0)
+                            {
+                                try
+                                {
+                                    c.Commit(cr);
+                                }catch(Exception ex)
+                                {
+                                    Console.WriteLine(ex);
+                                }
+                            }
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                    c.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Kafka_Samples/KafkaProducer/KafkaProducer/Program.cs
@@ -0,0 +1,60 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KafkaProducer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var conf = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                LingerMs = 2,
+                QueueBufferingMaxKbytes = 1048576,
+                 EnableSslCertificateVerification = true,
+             //   Debug="msg"
+                //QueueBufferingMaxMessages = 10000,
+               // BatchNumMessages = 1,
+            //  Acks = Acks.All
+            };
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.dat");
+            String info = File.ReadAllText(filePath);
+
+            Action <DeliveryReport<Null, string>> handler = r =>
+                Console.WriteLine(!r.Error.IsError
+                    ? $"Delivered message to {r.TopicPartitionOffset}"
+                    : $"Delivery Error: {r.Error.Reason}");
+
+            using (var p = new ProducerBuilder<Null, string>(conf).Build())
+            {
+                for (int i = 0; i < 10000; ++i)
+                {
+                 
+                    try
+                    {
+                        p.Produce("my-topic", new Message<Null, string> { Value = info }, handler);
+                        if(i % 50 == 0)
+                            p.Flush();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        Thread.Sleep(5000);
+                    }
+                }
+
+                // wait for up to 10 seconds for any inflight messages to be delivered.
+                p.Flush(TimeSpan.FromSeconds(10));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
