9d03629915c5c3621c925290263c52c796f453ba
Ajeesh B Nair <ajeeshklr@gmail.com>
2020-04-19 10:48:57 +0530

    Updating kafka configuration.

--- Kafka_Samples/KafkaConsumer/KafkaConsumer/Program.cs
+++ Kafka_Samples/KafkaConsumer/KafkaConsumer/Program.cs
@@ -1,7 +1,11 @@
 ﻿using Confluent.Kafka;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,41 +14,274 @@ namespace KafkaConsumer
 {
     class Program
     {
+        static string keys = string.Empty;
+
+        static byte[] kb = new byte[32];
+        static byte[] ib = new byte[16];
+        static RSACryptoServiceProvider provider = new RSACryptoServiceProvider();
+        static int blockSize = 0;
+        static RijndaelManaged rijAlg = new RijndaelManaged();
+
+
+        public static string DecryptData(byte[] data, byte[] key, byte[] iv)
+        {
+            string plaintext;
+
+
+
+            //rijAlg.Padding = PaddingMode.None;
+
+            // Create a decryptor to perform the stream transform.
+            ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+
+            // Create the streams used for decryption.
+            using (MemoryStream msDecrypt = new MemoryStream(data))
+            {
+                //msDecrypt.Position = 0;
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    //csDecrypt.FlushFinalBlock();
+
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt, Encoding.UTF8))
+                    {
+                        // Read the decrypted bytes from the decrypting stream
+                        // and place them in a string.
+                        plaintext = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+
+
+            return plaintext;
+        }
+
+
+        /// <summary>
+        /// Decrypt this message using this key
+        /// </summary>
+        /// <param name="dataToDecrypt">
+        /// The data To decrypt.
+        /// </param>
+        /// <param name="privateKeyInfo">
+        /// The private Key Info.
+        /// </param>
+        /// <returns>
+        /// The decrypted data.
+        /// </returns>
+        public static byte[] Decrypt(string str, RSACryptoServiceProvider key, int blockSize)
+        {
+            //// The bytearray to hold all of our data after decryption
+            byte[] decryptedBytes = { };
+
+            //Create a new instance of RSACryptoServiceProvider.
+            try
+            {
+                byte[] bytesToDecrypt = Convert.FromBase64String(str);
+                byte[] buffer = new byte[blockSize];
+
+                //// buffer containing decrypted information
+                byte[] decryptedBuffer = new byte[blockSize];
+
+                //// Initializes our array to make sure it can hold at least the amount needed to decrypt.
+                int totalBufferLength = bytesToDecrypt.Length;
+                int bytesAfterDecryption = (((totalBufferLength / blockSize) - 1) * (blockSize - 32)) + blockSize; // By this calculation, we don't have to allocate too much memory.
+                decryptedBytes = new byte[bytesAfterDecryption];
+                int offset = 0;
+
+                for (int i = 0; i < bytesToDecrypt.Length; i += blockSize)
+                {
+                    if (i + blockSize > bytesToDecrypt.Length)
+                    {
+                        blockSize = bytesToDecrypt.Length - i;
+                        buffer = new byte[blockSize];
+                    }
+
+                    Buffer.BlockCopy(bytesToDecrypt, i, buffer, 0, blockSize);
+                    decryptedBuffer = RSADecrypt(buffer, key, false);
+                    decryptedBuffer.CopyTo(decryptedBytes, offset);
+                    offset += decryptedBuffer.Length;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
+            return decryptedBytes;
+
+        }
+
+        public static byte[] RSADecrypt(byte[] DataToDecrypt, RSACryptoServiceProvider RSA, bool DoOAEPPadding)
+        {
+            try
+            {
+                return RSA.Decrypt(DataToDecrypt, DoOAEPPadding);
+            }
+            //Catch and display a CryptographicException  
+            //to the console.
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+
+                return null;
+            }
+        }
+
+        private static long totalEllapsed = 0;
+        private static int totalMessages = 0;
+        private static DateTime statisticsStartTime = DateTime.Now;
+        private static DateTime statisticsEndTime = DateTime.Now;
+        private static DateTime lastMessageReceivedTime = DateTime.Now;
+        private static int totalMessageAboveThreshold = 0;
+        static bool isTestingInProgress = false;
+        static long  maxDelay = 0;
+        static object lockObject = new object();
+        static int maxDelayThreshold = 100;
+
+        private static void ThreadExecutionHandler(object obj)
+        {
+            var timeWatch = new Stopwatch();
+            timeWatch.Start();
+
+            var data = obj as String;
+            // This data is in key#message format.
+            var keyvalues = data.Split(new char[] { '#' });
+
+            //Key, IV stored in first argument.
+            var encryptedKeys = keyvalues[0];
+            lock (lockObject)
+            {
+                if (!string.Equals(encryptedKeys, keys))
+                {
+                    var keyivs = encryptedKeys.Split(new char[] { ':' });
+
+                    var key = Decrypt(keyivs[0], provider, blockSize);
+                    var iv = Decrypt(keyivs[1], provider, blockSize);
+
+                    Buffer.BlockCopy(key, 0, kb, 0, 32);
+                    Buffer.BlockCopy(iv, 0, ib, 0, 16);
+                    keys = encryptedKeys;
+
+                    rijAlg.Key = kb;
+                    rijAlg.IV = ib;
+                }
+            }
+
+
+            byte[] dataInBytes = Convert.FromBase64String(keyvalues[1]);
+            var base64String = DecryptData(dataInBytes, kb, ib);
+
+            var actualData = Convert.FromBase64String(base64String);
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            while(stopWatch.ElapsedMilliseconds < 30)   
+            {
+                // Simulate heavy load.
+                continue;
+            }
+            stopWatch.Stop();
+            timeWatch.Stop();
+            var elapsedMilli = timeWatch.ElapsedMilliseconds;
+            lock (lockObject)
+            {
+                totalEllapsed += elapsedMilli;
+                totalMessages++;
+                if (elapsedMilli > maxDelay)
+                {
+                    maxDelay = elapsedMilli;
+                    Console.WriteLine($"Maximum delay now - {maxDelay}");
+                }
+            }
+
+
+
+            if(string.Equals(base64String, "LS0tU3RhcnQgb2YgVGVzdC0tLQ=="))
+            {
+                statisticsStartTime = DateTime.Now;
+                isTestingInProgress = true;
+            }else if(string.Equals(base64String, "LS0tRW5kIG9mIFRlc3QtLS0="))
+            {
+                statisticsEndTime = DateTime.Now;
+                isTestingInProgress = false;
+            }
+            if (elapsedMilli >= maxDelayThreshold)
+            {
+                // Console.WriteLine("Total time spent in handler - {0}ms", elapsedMilli); // Only print if elapsed is > 50ms
+                lock (lockObject)
+                {
+                    totalMessageAboveThreshold++;
+                }
+            }
+        }
+
         public static void Main(string[] args)
         {
+            provider.FromXmlString(File.ReadAllText("C:\\Projects\\keys\\key.xml"));
+            //// buffer to write byte sequence of the given block_size
+            blockSize = (provider.KeySize / 8);
+
+            rijAlg.Mode = CipherMode.ECB;
+            rijAlg.Padding = PaddingMode.ISO10126;
+
             var conf = new ConsumerConfig
             {
                 GroupId = "test-consumer-group",
                 BootstrapServers = "localhost:9093",
-                StatisticsIntervalMs = 120000,
-              //  QueuedMinMessages = 1000,
+                StatisticsIntervalMs = 10000,
+                //  QueuedMinMessages = 1000,
                 FetchWaitMaxMs = 5,
                 FetchErrorBackoffMs = 10,
-                EnableAutoCommit = false,
+                EnableAutoCommit = true,
                 SocketReceiveBufferBytes = 1048576,
                 SecurityProtocol = SecurityProtocol.Ssl,
                 EnableSslCertificateVerification = true,
                 SslCaLocation = "C:\\Projects\\keys\\cert-signed",
                 // Debug="consumer,topic",
-                // AutoCommitIntervalMs = 2,
+                AutoCommitIntervalMs = 10000,
+                
                 // Note: The AutoOffsetReset property determines the start offset in the event
                 // there are not yet any committed offsets for the consumer group for the
                 // topic/partitions of interest. By default, offsets are committed
                 // automatically, so in this example, consumption will only start from the
                 // earliest message in the topic 'my-topic' the first time you run the program.
-              //  AutoOffsetReset = AutoOffsetReset.Earliest
+                //  AutoOffsetReset = AutoOffsetReset.Earliest
             };
 
-            var commitPeriod = 1; // in ms ( for manual commit operation )
+            var commitPeriod = 10; // in ms ( for manual commit operation )
+
+            ThreadPool.SetMinThreads(8, 5);
+            ThreadPool.SetMaxThreads(10, 10);
 
             using (var c = new ConsumerBuilder<Ignore, string>(conf)
-                .SetStatisticsHandler((_, json) => Console.WriteLine($"Statistics: {json}"))
+                .SetStatisticsHandler((_, json) => {
+
+                    if (totalMessages > 0 && !isTestingInProgress )
+                    {
+
+                        var ellapsedTime = ((statisticsEndTime < lastMessageReceivedTime ? lastMessageReceivedTime : statisticsEndTime) - statisticsStartTime).TotalMilliseconds;
+                        Console.WriteLine($"Statistics: Aggregate time ellapsed - {totalEllapsed}ms, Total Messages - {totalMessages}, Average response - {totalEllapsed / totalMessages}ms");
+                        Console.WriteLine($"Statistics: Total time elapsed - {ellapsedTime}, Throughput - {totalMessages / (ellapsedTime / 1000)} messages/sec.,");
+                        Console.WriteLine($"Statistics: Total messages above {maxDelayThreshold}ms - {totalMessageAboveThreshold}");
+                        Console.WriteLine($"Last mesasge received time - {lastMessageReceivedTime}, Statistics ended time - {statisticsEndTime}");
+                        Console.WriteLine($"Maximum delay - {maxDelay}ms");
+
+                        totalEllapsed = 0;
+                        totalMessages = 0;
+                        totalMessageAboveThreshold = 0;
+                        maxDelay = 0;
+                    }
+
+
+                    })
                 .Build())
             {
                 c.Subscribe("my-topic");
 
                 CancellationTokenSource cts = new CancellationTokenSource();
-                Console.CancelKeyPress += (_, e) => {
+                Console.CancelKeyPress += (_, e) =>
+                {
                     e.Cancel = true; // prevent the process from terminating.
                     cts.Cancel();
                 };
@@ -64,18 +301,25 @@ namespace KafkaConsumer
                                 continue;
                             }
 
-                            Console.WriteLine($"Consumed message at: '{cr.TopicPartitionOffset}'.");
+                            var data = cr.Message.Value;
 
-                            if (cr.Offset % commitPeriod == 0)
-                            {
-                                try
-                                {
-                                    c.Commit(cr);
-                                }catch(Exception ex)
-                                {
-                                    Console.WriteLine(ex);
-                                }
-                            }
+                            ThreadPool.QueueUserWorkItem(ThreadExecutionHandler, data);
+                            lastMessageReceivedTime = DateTime.Now;
+
+                            //ThreadExecutionHandler(data);
+                            //  Console.WriteLine($"Consumed message at: '{cr.TopicPartitionOffset}'.");
+
+                            //if (cr.Offset % commitPeriod == 0)
+                            //{
+                            //    try
+                            //    {
+                            //        c.Commit(cr);
+                            //    }
+                            //    catch (Exception ex)
+                            //    {
+                            //        Console.WriteLine(ex);
+                            //    }
+                            //}
                         }
                         catch (ConsumeException e)
                         {
--- Kafka_Samples/KafkaProducer/KafkaProducer/Program.cs
+++ Kafka_Samples/KafkaProducer/KafkaProducer/Program.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,42 +13,202 @@ namespace KafkaProducer
 {
     class Program
     {
+        static RijndaelManaged rijAlg = null;
+        static ICryptoTransform encryptor = null;
+
+        public static byte[] EncryptData(string data)
+        {
+            byte[] encrypted;
+            // Create an RijndaelManaged object
+            // with the specified key and IV.
+
+            // Create the streams used for encryption.
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt, Encoding.UTF8))
+                    {
+                        //Write all data to the stream.
+                        swEncrypt.Write(data);
+                        swEncrypt.Close();
+                    }
+                    encrypted = msEncrypt.ToArray();
+                }
+                msEncrypt.Close();
+            }
+
+            return encrypted;
+        }
+
+        public static byte[] RSAEncrypt(byte[] DataToEncrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                //Create a new instance of RSACryptoServiceProvider.
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+
+                    //Import the RSA Key information. This only needs
+                    //toinclude the public key information.
+                    RSA.ImportParameters(RSAKeyInfo);
+
+                    //Encrypt the passed byte array and specify OAEP padding.  
+                    //OAEP padding is only available on Microsoft Windows XP or
+                    //later.  
+                    encryptedData = RSA.Encrypt(DataToEncrypt, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            //Catch and display a CryptographicException  
+            //to the console.
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+        }
+
+        public static byte[] Encrypt(byte[] encryptThis, RSACryptoServiceProvider provider)
+        {
+            try
+            {
+
+                int encryptedBufferSize = (provider.KeySize / 8);
+                int blockSize = encryptedBufferSize - 32;
+
+                //// buffer to write byte sequence of the given block_size
+                byte[] buffer = new byte[blockSize];
+
+                byte[] encryptedBuffer = new byte[encryptedBufferSize];
+
+                var totalBufferLength = ((encryptThis.Length / blockSize) * encryptedBufferSize) + ((encryptThis.Length % blockSize) > 0 ? encryptedBufferSize : 0);
+
+
+                //// Initializing our encryptedBytes array to a suitable size, depending on the size of data to be encrypted
+                byte[] encryptedBytes = new byte[totalBufferLength];
+                int offset = 0;
+
+                for (int i = 0; i < encryptThis.Length; i += blockSize)
+                {
+                    if (i + blockSize > encryptThis.Length)
+                    {
+                        blockSize = encryptThis.Length - i;
+                        buffer = new byte[blockSize];
+                    }
+
+                    //// encrypt the specified size of data, then add to final array.
+                    Buffer.BlockCopy(encryptThis, i, buffer, 0, blockSize);
+                    encryptedBuffer = RSAEncrypt(buffer, provider.ExportParameters(false), false);
+                    encryptedBuffer.CopyTo(encryptedBytes, offset);
+                    offset += encryptedBufferSize;
+                }
+
+                return encryptedBytes;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
         public static void Main(string[] args)
         {
+            var provider = new RSACryptoServiceProvider();
+            provider.FromXmlString(File.ReadAllText("C:\\Projects\\keys\\key.xml"));
+
+
             var conf = new ProducerConfig
             {
                 BootstrapServers = "localhost:9093",
-                LingerMs = 0,
+                LingerMs = 10,
                 QueueBufferingMaxKbytes = 1048576,
-                 EnableSslCertificateVerification = true,
-                 SecurityProtocol = SecurityProtocol.Ssl,
-                 SslCaLocation = "C:\\Projects\\keys\\cert-signed",
-                 //SslCertificateLocation = "C:\\Projects\\kafka_repo\\keys\\pub.pem",
-                 //SslKeyLocation= "C:\\Projects\\kafka_repo\\keys\\pri.key",
-                 //SslKeyPassword = "password123",
+                MessageMaxBytes = 50 * 1024,
+                EnableSslCertificateVerification = true,
+                SecurityProtocol = SecurityProtocol.Ssl,
+                SslCaLocation = "C:\\Projects\\keys\\cert-signed",
+                Partitioner = Partitioner.Consistent,
+                //SslCertificateLocation = "C:\\Projects\\kafka_repo\\keys\\pub.pem",
+                //SslKeyLocation= "C:\\Projects\\kafka_repo\\keys\\pri.key",
+                //SslKeyPassword = "password123",
                 //   Debug="msg"
                 //QueueBufferingMaxMessages = 10000,
                 // BatchNumMessages = 1,
                 //  Acks = Acks.All
             };
             var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.dat");
-            String info = File.ReadAllText(filePath);
+            //byte[] info = Encoding.ASCII.GetBytes("Ajeesh B Nair");
+            //byte[] info = Encoding.UTF8.GetBytes("Ajeesh B Nair asdddddddddddddddddddddddddddddddddddddddddddd asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddassssssssqwdqw-Ajeesh B Nair asdddddddddddddddddddddddddddddddddddddddddddd asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddassssssssqwdqw");//  File.ReadAllBytes(filePath);
+            byte[] info = File.ReadAllBytes(filePath);
+            var startMessage = "---Start of Test---";
+            var endingMessage = "---End of Test---";
+            var startMessageBytes = Encoding.ASCII.GetBytes(startMessage);
+            var endMessageBytes = Encoding.ASCII.GetBytes(endingMessage);
 
-            Action <DeliveryReport<Null, string>> handler = r =>
-                Console.WriteLine(!r.Error.IsError
-                    ? $"Delivered message to {r.TopicPartitionOffset}"
-                    : $"Delivery Error: {r.Error.Reason}");
+            var b64StartMessage = Convert.ToBase64String(startMessageBytes);
+            var b64EndMessage = Convert.ToBase64String(endMessageBytes);
+
+            var base64String = Convert.ToBase64String(info);
+
+            rijAlg = new RijndaelManaged();
+            rijAlg.Mode = CipherMode.ECB;
+            rijAlg.Padding = PaddingMode.ISO10126;
+
+            encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+            var key = rijAlg.Key;
+            var iv = rijAlg.IV;
+
+
+            // Encrypt key and iv using RSA
+            var encryptedK = Convert.ToBase64String(Encrypt(key, provider), System.Base64FormattingOptions.None);
+            var encryptedIV = Convert.ToBase64String(Encrypt(iv, provider), System.Base64FormattingOptions.None);
+
+            var messageKey = string.Format("{0}:{1}", encryptedK, encryptedIV);
+
+
+
+            Action<DeliveryReport<Null, string>> handler = r =>
+           Console.WriteLine(!r.Error.IsError
+               ? $"Delivered message to {r.TopicPartitionOffset}"
+               : $"Delivery Error: {r.Error.Reason}");
 
             using (var p = new ProducerBuilder<Null, string>(conf).Build())
             {
+                var startTime = DateTime.Now.Ticks;
+                
                 for (int i = 0; i < 10000; ++i)
                 {
-                 
                     try
                     {
-                        p.Produce("my-topic", new Message<Null, string> { Value = info }, handler);
-                        //if(i % 50 == 0)
-                        //    p.Flush();
+                        // Encrypt data using AES
+
+                        string dataToEncrypt;
+                        if (i == 0)
+                        {
+                            dataToEncrypt = b64StartMessage;
+                        }
+                        else if (i == 9999)
+                        {
+                            dataToEncrypt = b64EndMessage;
+                        }
+                        else
+                        {
+                            dataToEncrypt = base64String;
+                        }
+                        var encryptedValue = EncryptData(dataToEncrypt);
+                        var message = String.Format("{0}#{1}", messageKey, Convert.ToBase64String(encryptedValue));
+                        p.Produce("my-topic", new Message<Null, string> { Value = message }, null);
+
+                        if (i % 49 == 0)
+                        {
+                            p.Flush();
+                            Thread.Sleep(100);
+                        }
+
                     }
                     catch (Exception ex)
                     {
@@ -54,6 +216,10 @@ namespace KafkaProducer
                         Thread.Sleep(5000);
                     }
                 }
+
+                var endTime = DateTime.Now.Ticks;
+                var totalTimeTaken = (endTime - startTime) / TimeSpan.TicksPerMillisecond;
+                Console.WriteLine("Total time took to produce 10000 messages is {0}ms\r\n", totalTimeTaken);
 
                 // wait for up to 10 seconds for any inflight messages to be delivered.
                 p.Flush(TimeSpan.FromSeconds(10));
